9cb306852cebede1a5c97cf4eddfb65773cbb59b
VulgarGenius <262869122@qq.com>
2021-09-24 11:25:21 +0800

    Add return value in LunarMonth.fromYm
    Without return value, the LunarMonth object is always empty

--- lib/calendar/LunarMonth.dart
+++ lib/calendar/LunarMonth.dart
@@ -19,7 +19,7 @@ class LunarMonth {
   LunarMonth(this._year, this._month, this._dayCount, this._firstJulianDay);
 
   LunarMonth.fromYm(int lunarYear, int lunarMonth) {
-    LunarYear.fromYear(lunarYear).getMonth(lunarMonth);
+    return LunarYear.fromYear(lunarYear).getMonth(lunarMonth);
   }
 
   int getYear() => _year;
